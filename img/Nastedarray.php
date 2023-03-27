2fad94989c7e4dc127d27343d057c17515343def
Hafiz Ahmed Ali Ansari <117213903+HafizZada@users.noreply.github.com>
2023-03-27 14:21:34 +0500

    Maria.B Website

---
+++ img/Nastedarray.php
@@ -0,0 +1,12 @@
+<?php
+$Ahmed=array(
+  array ("01","Ahmed","B.C.A","Canada",  "file:///E:/my%20desktop%20folder/Ahmed%20Websites/img/my.jpeg"),
+  array ("02","Sajjad","B.C.A","Amearica", "file:///E:/my%20desktop%20folder/Ahmed%20Websites/img/my.jpeg"),
+  array ("03","Asharif","B.C.A","New York", "file:///E:/my%20desktop%20folder/Ahmed%20Websites/img/my.jpeg"),
+  array ("04","Ali","B.C.A","Saudia Arab",    "file:///E:/my%20desktop%20folder/Ahmed%20Websites/img/my.jpeg"),   
+  array ("01","Kamran","B.C.A","Swizerland",  "file:///E:/my%20desktop%20folder/Ahmed%20Websites/img/my.jpeg")
+  )
+echo"Empolyee ID:"."$Ahmed[0][0]."Name:".$Ahmed[0][1]."Education:"
+.$Ahmed[0][2]." City:".$Ahmed[0][3]."<img width='300px',height='300px'src='
+ 
+?>
